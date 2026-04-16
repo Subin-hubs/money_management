af4a74d28ca0a16bbcf35194da789383037203a6
Subin-hubs <subintamang008@gmail.com>
2026-04-16 22:55:07 +0545

    ADD NOTIFICATATION

---
+++ lib/features/notficatation/notificatation_service.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  static Future<void> init() async {
+    const AndroidInitializationSettings androidInit =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings settings =
+    InitializationSettings(android: androidInit);
+
+    await _notificationsPlugin.initialize(settings: settings);
+  }
+
+
+
+  static Future<void> showNotification({
+    required String title,
+    required String body,
+  }) async {
+    const AndroidNotificationDetails androidDetails =
+    AndroidNotificationDetails(
+      'main_channel',
+      'Main Notifications',
+      importance: Importance.high,
+      priority: Priority.high,
+      playSound: true,
+    );
+
+    const NotificationDetails details =
+    NotificationDetails(android: androidDetails);
+
+    print("Notification triggered");
+    final int id = DateTime.now().millisecondsSinceEpoch;
+
+    await _notificationsPlugin.show(
+      id: id,
+      title: title,
+      body: body,
+      notificationDetails: details,
+    );
+  }
+}
